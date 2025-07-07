2aa302745c0ec4c79102af9e652a3676f5bbda38
saulsayerzgdp <saul.sayers@gdplabs.id>
2025-07-07 18:03:04 +0700

    test

--- src/hello_world/crew.py
+++ src/hello_world/crew.py
@@ -1,54 +1,60 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-from crewai.agents.agent_builder.base_agent import BaseAgent
-from typing import List
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
-
-
+from crewai_tools import SerperDevTool  # Import Serper tool
 
 @CrewBase
 class HelloWorld():
-    """HelloWorld crew"""
+    agents: list
+    tasks: list
 
-    agents: List[BaseAgent]
-    tasks: List[Task]
-
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
-    def hello_world_agent(self) -> Agent:
+    def research_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['hello_world_agent'], # type: ignore[index]
+            config=self.agents_config['research_agent'],
             verbose=True,
             llm='gpt-4.1'
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @agent
+    def web_search_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['web_search_agent'],
+            tools=[SerperDevTool()],  # Attach the Serper tool
+            verbose=True,
+            llm='gpt-4.1'
+        )
+
+    @agent
+    def information_compiler_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['information_compiler_agent'],
+            verbose=True,
+            llm='gpt-4.1'
+        )
+
     @task
-    def hello_world_task(self) -> Task:
+    def web_search_task(self) -> Task:
         return Task(
-            config=self.tasks_config['hello_world_task'], # type: ignore[index]
-            tools=[]
+            config=self.tasks_config['web_search_task']
+        )
+
+    @task
+    def information_compiler_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['information_compiler_task']
+        )
+
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task']
         )
 
     @crew
     def crew(self) -> Crew:
-        """Creates the HelloWorld crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
-            verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            verbose=True
         )
